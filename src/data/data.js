b62b42d2dbd019686efe8e8754c1d4d831cdec42
Scott Sutherland <scott@webcheddar.ca>
2023-01-05 17:59:04 -0500

    Updating directory page with directory component and category component

---
+++ src/data/data.js
@@ -0,0 +1,29 @@
+const categories = [
+    {
+      id: 1,
+      title: 'Hats',
+      imageUrl: 'https://media.gq.com/photos/5a04f9a398002d2e253679f5/master/pass/fall-hats-gq-style-0816-01-1.jpg',
+    },
+    {
+      id: 2,
+      title: 'Jackets',
+      imageUrl: 'https://i.insider.com/631f5256e8b500001850d09a?width=1000&format=jpeg&auto=webp',
+    },
+    {
+      id: 3,
+      title: 'Sneakers',
+      imageUrl: 'https://nypost.com/wp-content/uploads/sites/2/2021/09/shoes-1.jpg?quality=75&strip=all',
+    },
+    {
+      id: 4,
+      title: 'Womens',
+      imageUrl: 'https://cdn.shopify.com/s/files/1/0441/3580/9179/files/Fall_ClothesMentor_ShopOnline_bf6137e1-379d-4e19-bb6c-7b96888e6918.jpg?v=1664891185',
+    },
+    {
+      id: 5,
+      title: 'Mens',
+      imageUrl: 'https://www.dmarge.com/wp-content/uploads/2021/10/menswear-brands.jpg',
+    }
+  ]
+
+  export default categories;
