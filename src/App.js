dd18aa094bf210e435b92459f91a8f40603f076a
Scott Sutherland <scott@webcheddar.ca>
2023-02-17 16:11:05 -0500

    created authentication page component alongside sign in form

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 
 import Home from "./routes/home/home";
 import Navigation from './routes/navigation/navigation';
-import SignIn from './routes/sign-in/sign-in';
+import Authentication from './routes/authentication/authentication.component';
 
 const Shop = () => {
   return <h1>This is the SHOP page</h1>
@@ -14,7 +14,7 @@ const App = () => {
       <Route path='/' element={<Navigation />}>
         <Route index element={<Home />} />
         <Route path='shop' element={<Shop />} />
-        <Route path='sign-in' element={<SignIn />} />
+        <Route path='auth' element={<Authentication />} />
       </Route>
     </Routes>    
   );
